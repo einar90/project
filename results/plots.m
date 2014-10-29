0ea7560b6bb9b8ab83a0cecf2b0957f8cac0d6a4
Einar Baumann <einar.baumann@gmail.com>
2014-10-29 11:08:20 +0100

    Change to metric results

--- results/plots.m
+++ results/plots.m
@@ -1,31 +1,41 @@
 clear all; close all; clc;
 
 % Conversion factors
-bblDay2ccSec = 1.84;
-ft2cm = 30.48;
-psia2atm = 0.06805;
+sm3PerDay2ccPerSec = 11.57;
+mD2D = 0.001;
+m2cm = 100.0;
+bar2atm = 0.9869;
 
 % Parameters
-q = 1000*bblDay2ccSec; % cc/sec
-mu = 0.96; % cP
-k = 300.0e-3; % D
-h = 100.0*ft2cm; % cm
+q = 150*sm3PerDay2ccPerSec; % cc/sec
+mu = 0.5; % cP
+k = 300.0*mD2D; % D
+h = 30*m2cm; % cm
 
-factor = q*mu / (k*h);
+factor = (k*h)/(q*mu);
 
 %% Eclipse 10x10
-p_10x10 = load('eclipse/10x10-pressure.dat')*psia2atm; % psia
+p_10x10 = load('eclipse/10x10-pressure.dat').*bar2atm;
 p_0 = p_10x10(1,1); % Well-block pressure
 
-p = (p_10x10-p_0)./factor;
+p = (p_10x10-p_0).*factor;
 
 x = [];
 for i=1:10
   for j=1:10
-    x(i,j) = sqrt(i^2+j^2);
-    %p = [p (p_10x10(i,j)-p_0)/factor];
+    x(i,j) = sqrt((i-1)^2+(j-1)^2);
   end
 end
+
+% Regression line for diagonal
+for i=1:4
+    x_diag(i) = sqrt((i-1)^2+(i-1)^2);
+end
+pfit = polyfit(x(2:4,2:4), p(2:4,2:4), 1);
+pval = polyval(pfit, x_diag);
     
-%semilogx(x,p,'.')
-plot(x(2,2), p(2,2),'.')
+
+semilogx(x(2:4,2:4), p(2:4,2:4),'.')
+hold on;
+semilogx(x_diag,pval)
+axis([0 6 0 2.5]);
