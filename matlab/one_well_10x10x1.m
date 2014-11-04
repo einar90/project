8bbfd9e82d62fb22794a4bb1c36f2578b8f66778
Einar Baumann <einar.baumann@gmail.com>
2014-11-04 17:02:40 +0100

    Still working

--- matlab/one_well_10x10x1.m
+++ matlab/one_well_10x10x1.m
@@ -1,4 +1,4 @@
-clear all; clc;
+clear all; clc; close all;
 %% Define grid, rock and fluid data
 dim = 10;
 G = cartGrid([dim, dim, 1]);
@@ -9,7 +9,7 @@ fluid     = initSingleFluid('mu' ,    1*centi*poise     , ...
 T = computeTrans(G, rock, 'Verbose', true);
 
 %% Wells
-r = .1;
+r = .3;
 W = [];
 
 %% Production well
@@ -34,12 +34,6 @@ resSol = incompTPFA(resSol, G, T, fluid, 'wells', W);
 %% Create 2D matrix from pressure solution vector
 P = reshape(convertTo(resSol.pressure, atm), dim, dim);
 
-%% Pressures on diagonal from producer towards center
-for i = 2:dim/2
-  diagP(i-1) = P(i,i);
-  r(i-1) = sqrt(2*(i^2));
-end
-
 %% Pressure line for plotting
 sm3PerDay2ccPerSec = 11.57;
 k = 100*0.001;
@@ -55,13 +49,16 @@ for i=1:dim
     end
 end
 
-% Cut matrices
-p_plot = p_plot(2:3,2:3);
-x = x(2:3,2:3);
+
 
 %% Regression line
-pfit = polyfit(x, p_plot, 1);
-pval = polyval(pfit, x);
+xx = x(1:floor(dim/2),1:floor(dim/2));
+pp = p_plot(1:dim/2,1:dim/2);
+xx = xx(:);
+pp = pp(:);
+pfit = polyfit(log(xx(2:end)), pp(2:end), 1);
+pval = polyval(pfit, xx);
+x_reg = [.1:.01:10];
 
 %% Report results
 subplot(1,2,1)
@@ -70,9 +67,11 @@ subplot(1,2,1)
   view(2), axis tight, colorbar
   
 subplot(1,2,2)
-  plot(x, p_plot, '*')
+  semilogx(xx, pp, '*')
   title('Pressure vs. radius')
   xlabel('$$r/\Delta x = \sqrt{i^2 + j^2}$$','interpreter','latex')
   ylabel('$$P$$' ,'interpreter','latex')
-  ylim([0 max(diagP)]);
-  xlim([0, 6])
+  ylim([0 5]);
+  xlim([10e-2, 2e1])
+  hold on 
+  semilogx(x_reg, pfit(2)+pfit(1).*log(x_reg))
