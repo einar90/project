d480ba5043a53a555c7ccc783ad25f68b1ef8a7e
Einar Baumann <einar.baumann@gmail.com>
2014-11-13 21:27:59 +0100

    Calculating r_eq from peaceman solution

--- results/process_results.py
+++ results/process_results.py
@@ -1,47 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import json  # Only used for prettyprinting
+
+pcm_file = 'peaceman/32x32-pressure.dat'   # Peaceman solver results
+ecl_file = 'eclipse/11x11-pressure.dat'    # ECL100 simulation results
+
+p = {
+    'ecl': np.loadtxt(ecl_file)*0.986923267,  # bar -> atm
+    'pcm': np.loadtxt(pcm_file)               # Dimensionless
+}
+
+M = {
+    'ecl': int(np.sqrt(p.get('ecl').size))-1,
+    'pcm': int(np.sqrt(p.get('pcm').size))-1
+}
 
 props = {
     'ecl': {
-        'k':  300.0/1000.0,      # D
-        'h':  30.0*100.0,        # cm
-        'q':  150.0*11.5740741,  # cc/sec
+        'k':  300.0/1000.0,      # mD -> D
+        'h':  30.0*100.0,        # m -> cm
+        'q':  150.0*11.5740741,  # stb/day -> cc/sec
         'mu': 0.5,               # cP
-        'dx': 30.0*100.0,        # cm
+        'dx': 30.0*100.0,        # m -> cm
+        'p_prod': p.get('ecl')[0, 0],
+        'p_inj':  p.get('ecl')[M.get('ecl'), M.get('ecl')],
+    },
+    'pcm': {                     # Dimensionless
+        'k':  1.0,
+        'h':  1.0,
+        'q':  1.0,
+        'mu': 1.0,
+        'dx': 1.0,
+        'p_prod': p.get('pcm')[0, 0],
+        'p_inj':  p.get('pcm')[M.get('pcm'), M.get('pcm')],
     }
 }
 
 
-def r_eq_exact(M, p_prod, p_inj, props):
-    print props
-    print 'p_00: ', p_prod
-    print 'p_MM: ', p_inj
-    return np.sqrt(2.0) * float(M) * np.exp(-np.pi * props.get('k') * props.get('h')
-                                            / (props.get('q') * props.get('mu'))
-                                            * (p_inj - p_prod)
-                                            - 0.6190
-                                            )
-
-p = {
-    'ecl': np.loadtxt('eclipse/11x11-pressure.dat')*0.986923267  # atm
-}
-
-M = {
-    'ecl': int(np.sqrt(p.get('ecl').size))-1
-}
+def r_eq_exact(M, props):
+    return np.sqrt(2.0) * float(M) * np.exp(
+        - np.pi * props.get('k') * props.get('h')
+        / (props.get('q') * props.get('mu'))
+        * (props.get('p_inj') - props.get('p_prod'))
+        - 0.6190
+    )
 
 
 r_eq = {
     'ecl': {
-        'exact': r_eq_exact(
-            M.get('ecl'),
-            p.get('ecl')[0, 0],
-            p.get('ecl')[M.get('ecl'), M.get('ecl')],
-            props.get('ecl')
-        )
+        'exact': r_eq_exact(M.get('ecl'), props.get('ecl'))
+    },
+    'pcm': {
+        'exact': r_eq_exact(M.get('pcm'), props.get('pcm'))
     }
 }
 
-print M
-#print p
-print r_eq
+print 'M: ', M
+print json.dumps(props, sort_keys=True, indent=2)
+print 'Calculated r_eq: ', r_eq
