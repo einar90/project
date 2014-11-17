6d3ec368baaa0fef02b5f00d0051e29343980f67
Einar Baumann <einar.baumann@gmail.com>
2014-11-17 14:56:17 +0100

    Remove plotting

--- matlab/one_well_10x10x1.m
+++ matlab/one_well_10x10x1.m
@@ -31,49 +31,6 @@ resSol = initState(G, W, 0);
 gravity off
 resSol = incompTPFA(resSol, G, T, fluid, 'wells', W);
 
-%% Create 2D matrix from pressure solution vector
+%% Create 2D matrix from pressure solution vector and save it
 P = reshape(convertTo(resSol.pressure, atm), dim, dim);
 save([num2str(dim) 'x' num2str(dim) '-pressure.dat'], 'P', '-ascii')
-
-
-%% Pressure line for plotting
-sm3PerDay2ccPerSec = 11.57;
-k = .3; % D
-h = 30.0*100; % cm
-q = 150.0*sm3PerDay2ccPerSec; % cc/sec
-mu = .5; % cP
-factor = k*h/(q*mu);
-p_plot = (P - P(1,1)).*factor;
-
-for i=1:dim
-    for j=1:dim
-        x(i,j) = sqrt((i-1)^2 + (j-1)^2);
-    end
-end
-
-
-
-%% Regression line
-xx = x(1:floor(dim/2),1:floor(dim/2));
-pp = p_plot(floor(1:dim/2),floor(1:dim/2));
-xx = xx(:);
-pp = pp(:);
-pfit = polyfit(log(xx(2:end)), pp(2:end), 1);
-pval = polyval(pfit, xx);
-x_reg = [.1:.01:10];
-
-%% Report results
-subplot(1,2,1)
-  surf(P)
-  title('Pressure [bar]')
-  view(2), axis tight, colorbar
-  
-subplot(1,2,2)
-  semilogx(xx, pp, '*')
-  title('Pressure vs. radius')
-  xlabel('$$r/\Delta x = \sqrt{i^2 + j^2}$$','interpreter','latex')
-  ylabel('$$P$$' ,'interpreter','latex')
-  ylim([0 5]);
-  xlim([10e-2, .6e1])
-  hold on 
-  semilogx(x_reg, pfit(2)+pfit(1).*log(x_reg))
