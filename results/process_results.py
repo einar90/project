5fa8978055d72f679d553e37759e8ac0bec516a9
Einar Baumann <einar.baumann@gmail.com>
2014-11-20 11:40:44 +0100

    Change dictionary access method

--- results/process_results.py
+++ results/process_results.py
@@ -21,9 +21,9 @@ props = {
         'q':  150.0*11.5740741,   # m3/day -> cc/sec
         'mu': 0.5,                # cP
         'dx': 30.0*100.0,         # m -> cm
-        'M': int(np.sqrt(p.get('ecl').size))-1,
-        'p_prod': p.get('ecl')[0, 0],
-        'p_inj':  p.get('ecl')[-1, -1],
+        'M': int(np.sqrt(p['ecl'].size))-1,
+        'p_prod': p['ecl'][0, 0],
+        'p_inj':  p['ecl'][-1, -1],
     },
     'ecll': {
         'k':  300.0/1000.0,       # mD -> D
@@ -31,9 +31,9 @@ props = {
         'q':  50000.0/60.0/60.0,  # cc/hr -> cc/sec
         'mu': 0.5,                # cP
         'dx': 30.0,               # cm
-        'M': int(np.sqrt(p.get('ecll').size))-1,
-        'p_prod': p.get('ecll')[0, 0],
-        'p_inj':  p.get('ecll')[-1, -1],
+        'M': int(np.sqrt(p['ecll'].size))-1,
+        'p_prod': p['ecll'][0, 0],
+        'p_inj':  p['ecll'][-1, -1],
     },
     'mrst': {
         'k':  .3,                 # D
@@ -41,9 +41,9 @@ props = {
         'q':  150.0*11.5740741,   # m3/day -> cc/sec
         'mu': 0.5,                # cP
         'dx': 30.0*100,           # m -> cm
-        'M': int(np.sqrt(p.get('mrst').size))-1,
-        'p_prod': p.get('mrst')[0, 0],
-        'p_inj':  p.get('mrst')[-1, -1],
+        'M': int(np.sqrt(p['mrst'].size))-1,
+        'p_prod': p['mrst'][0, 0],
+        'p_inj':  p['mrst'][-1, -1],
     },
     'pcm': {                      # Dimensionless
         'k':  1.0,
@@ -51,37 +51,37 @@ props = {
         'q':  1.0,
         'mu': 1.0,
         'dx': 1.0,
-        'M': int(np.sqrt(p.get('pcm').size))-1,
-        'p_prod': p.get('pcm')[0, 0],
-        'p_inj':  p.get('pcm')[-1, -1],
+        'M': int(np.sqrt(p['pcm'].size))-1,
+        'p_prod': p['pcm'][0, 0],
+        'p_inj':  p['pcm'][-1, -1],
     }
 }
 
 
 def r_eq_exact(props):
-    return np.sqrt(2.0) * float(props.get('M')) * np.exp(
-        - np.pi * props.get('k') * props.get('h')
-        / (props.get('q') * props.get('mu'))
-        * (props.get('p_inj') - props.get('p_prod'))
+    return np.sqrt(2.0) * float(props['M']) * np.exp(
+        - np.pi * props['k'] * props['h']
+        / (props['q'] * props['mu'])
+        * (props['p_inj'] - props['p_prod'])
         - 0.6190
     )
 
 
 def r_eq_regression(props, p):
     # calculate dimensionless pressure and pressure difference
-    p_D = p * props.get('k') * props.get('h') \
-        / (props.get('q') * props.get('mu'))
-    p_diff = (p_D - p_D[0, 0])[0:props.get('M')/2, 0:props.get('M')/2]
+    p_D = p * props['k'] * props['h'] \
+        / (props['q'] * props['mu'])
+    p_diff = (p_D - p_D[0, 0])[0:props['M']/2, 0:props['M']/2]
 
     # compute radius matrix
-    r = np.zeros([props.get('M')/2, props.get('M')/2])
-    for i in range(props.get('M')/2):
-        for j in range(props.get('M')/2):
+    r = np.zeros([props['M']/2, props['M']/2])
+    for i in range(props['M']/2):
+        for j in range(props['M']/2):
             r[i, j] = np.sqrt(i**2 + j**2)
 
     # linearize matrices and remove well-block
-    r = r.reshape([(props.get('M')/2)**2, ])[1:]
-    p_diff = p_diff.reshape([(props.get('M')/2)**2, ])[1:]
+    r = r.reshape([(props['M']/2)**2, ])[1:]
+    p_diff = p_diff.reshape([(props['M']/2)**2, ])[1:]
 
     # create regression line and -polynomial
     reg = np.polyfit(np.log(r), p_diff, deg=1)
@@ -97,15 +97,15 @@ def make_plots(props, p, r_eq, title):
     r_reg_extended = [r/10.0 for r in r_reg_extended]
 
     # compute full radius matrix
-    r = np.zeros([props.get('M'), props.get('M')])
-    for i in range(props.get('M')):
-        for j in range(props.get('M')):
+    r = np.zeros([props['M'], props['M']])
+    for i in range(props['M']):
+        for j in range(props['M']):
             r[i, j] = np.sqrt(i**2 + j**2)
 
     # plot regression
-    titlestring = title + ', M = ' + str(props.get('M')) \
-                        + '; exact: ' + str(r_eq.get('exact')) \
-                        + '; regression: ' + str(r_eq.get('regression'))
+    titlestring = title + ', M = ' + str(props['M']) \
+                        + '; exact: ' + str(r_eq['exact']) \
+                        + '; regression: ' + str(r_eq['regression'])
     plt.figure(titlestring)
     ax1 = plt.subplot(1, 2, 1)
     ax1.scatter(r_reg, p_diff)
@@ -125,41 +125,40 @@ def make_plots(props, p, r_eq, title):
 
 r_eq = {
     'ecl': {
-        'exact': r_eq_exact(props.get('ecl')),
+        'exact': r_eq_exact(props['ecl']),
         'regression': r_eq_regression(
-            props.get('ecl'), p.get('ecl')
+            props['ecl'], p['ecl']
         )[0]
     },
     'ecll': {
-        'exact': r_eq_exact(props.get('ecll')),
+        'exact': r_eq_exact(props['ecll']),
         'regression': r_eq_regression(
-            props.get('ecll'), p.get('ecll')
+            props['ecll'], p['ecll']
         )[0]
     },
     'mrst': {
-        'exact': r_eq_exact(props.get('mrst')),
+        'exact': r_eq_exact(props['mrst']),
         'regression': r_eq_regression(
-            props.get('mrst'), p.get('mrst')
+            props['mrst'], p['mrst']
         )[0]
     },
     'pcm': {
-        'exact': r_eq_exact(props.get('pcm')),
+        'exact': r_eq_exact(props['pcm']),
         'regression': r_eq_regression(
-            props.get('pcm'), p.get('pcm')
+            props['pcm'], p['pcm']
         )[0]
     }
 }
 
 
-print 'M: ', props.get('M')
 print json.dumps(props, sort_keys=True, indent=2)
 print 'Calculated r_eq: '
 print json.dumps(r_eq, sort_keys=True, indent=2)
 
-make_plots(props.get('ecl'), p.get('ecl'), r_eq.get('ecl'), 'ECL100 Metric')
-make_plots(props.get('ecll'), p.get('ecll'), r_eq.get('ecll'), 'ECL100 Lab')
-make_plots(props.get('mrst'), p.get('mrst'), r_eq.get('mrst'), 'MRST')
-make_plots(props.get('pcm'), p.get('pcm'), r_eq.get('pcm'), 'Peaceman')
+make_plots(props['ecl'], p['ecl'], r_eq['ecl'], 'ECL100 Metric')
+make_plots(props['ecll'], p['ecll'], r_eq['ecll'], 'ECL100 Lab')
+make_plots(props['mrst'], p['mrst'], r_eq['mrst'], 'MRST')
+make_plots(props['pcm'], p['pcm'], r_eq['pcm'], 'Peaceman')
 
 plt.draw()
 plt.show()
